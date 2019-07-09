ed210729c29293254f11c3d98b7d42f395cfd258
lenovo <quanquanting@qq.com>
2019-07-09 22:03:04 +0800

    1.添加文件

--- pdf2txt.py
+++ pdf2txt.py
@@ -8,41 +8,95 @@
 # @Description:
 #
 #
-from pdfminer.pdfparser import PDFParser
-from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfpage import PDFTextExtractionNotAllowed
-from pdfminer.pdfinterp import PDFResourceManager
-from pdfminer.pdfinterp import PDFPageInterpreter
-from pdfminer.pdfdevice import PDFDevice
-from pdfminer.layout import *
-from pdfminer.converter import PDFPageAggregator
-import os
+# ! python3
+# -*- coding: utf-8 -*-
 
-fp = open('test.pdf', 'rb')
-# 来创建一个pdf文档分析器
-parser = PDFParser(fp)
-# 创建一个PDF文档对象存储文档结构
-document = PDFDocument(parser)
-# 检查文件是否允许文本提取
-if not document.is_extractable:
-    raise PDFTextExtractionNotAllowed
-else:
-    # 创建一个PDF资源管理器对象来存储共赏资源
-    rsrcmgr = PDFResourceManager()
-    # 设定参数进行分析
-    laparams = LAParams()
-    # 创建一个PDF设备对象
-    # device=PDFDevice(rsrcmgr)
-    device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-    # 创建一个PDF解释器对象
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    # 处理每一页
-    for page in PDFPage.create_pages(document):
-        interpreter.process_page(page)
-        # 接受该页面的LTPage对象
-        layout = device.get_result()
-        for x in layout:
-            if (isinstance(x, LTTextBoxHorizontal)):
-                with open('a.txt', 'a') as f:
-                    f.write(x.get_text().encode('utf-8') + '\n')
+import sys
+import importlib
+import requests
+
+importlib.reload(sys)
+
+from pdfminer.pdfparser import PDFParser, PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.layout import LTTextBoxHorizontal, LAParams
+from pdfminer.pdfinterp import PDFTextExtractionNotAllowed
+
+
+def parse(readPath, savePath):
+    fp = open(readPath, 'rb')  # 以二进制读模式打开
+    # fp = urlopen(path)  # 远程文件
+
+    # 用文件对象来创建一个pdf文档分析器
+    praser = PDFParser(fp)
+    # 创建一个PDF文档
+    doc = PDFDocument()
+    # 连接分析器 与文档对象
+    praser.set_document(doc)
+    doc.set_parser(praser)
+    # 提供初始化密码
+    # 如果没有密码 就创建一个空的字符串
+    doc.initialize()
+
+    # 检测文档是否提供txt转换，不提供就忽略
+    if not doc.is_extractable:
+        raise PDFTextExtractionNotAllowed
+    else:
+        # 创建PDf 资源管理器 来管理共享资源
+        rsrcmgr = PDFResourceManager()
+        # 创建一个PDF设备对象
+        laparams = LAParams()
+
+        device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+        # 创建一个PDF解释器对象
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+        # 循环遍历列表，每次处理一个page的内容
+        for page in doc.get_pages():  # doc.get_pages() 获取page列表
+            interpreter.process_page(page)
+            # 接受该页面的LTPage对象
+            layout = device.get_result()
+            # 这里layout是一个LTPage对象 里面存放着 这个page解析出的各种对象 一般包括LTTextBox, LTFigure, LTImage, LTTextBoxHorizontal 等等 想要获取文本就获得对象的text属性，
+            for out in layout:
+                if (isinstance(out, LTTextBoxHorizontal)):
+                    with open(savePath, 'a+', encoding='utf-8') as f:
+                        results = out.get_text()
+                        print(results)
+                        f.write(results + '\n')
+
+
+def aconvert(filename, filepath):
+    "https://www.aconvert.com/cn/pdf/"
+    post_pdf_url = 'https://s2.aconvert.com/convert/convert-batch-win.php'
+    headers = {
+        'Host': 's2.aconvert.com',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36',
+        'Accept': '*/*',
+        'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Referer': 'https://www.aconvert.com/cn/pdf/',
+        'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundary99PEndn49fN432TM',
+        'Content-Length': '738436',
+        'Origin': 'https://www.aconvert.com',
+        'Connection': 'keep-alive',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+    }
+    files = {
+        'file': (filename, open(filepath, 'rb'), 'application/pdf'),
+    }
+    data = {
+        'targetformat': 'txt',
+        'filelocation': 'local',
+    }
+    res = requests.post(post_pdf_url, headers=headers, files=files, data=data)
+    print(res.text)
+
+
+if __name__ == '__main__':
+    readPath = r'E:\QuanQTing Files\Documents\Code\Python Project\Company_Annual_Report_Analysis_TF\000027深圳能源：2016年度社会责任报告2017-04-08(2)(1).PDF'
+    filename = readPath.split('\\')[-1]
+    savePath = readPath.replace('PDF', 'txt').replace('pdf', 'txt')
+    aconvert(filename, readPath)
+    parse(readPath, savePath)
